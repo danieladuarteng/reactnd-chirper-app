c074ec10d2124210dc82f4faccbd6e4caddba152
Daniela Duarte <falecom@danieladuarte.com.br>
2019-02-10 09:04:30 -0200

    feat: call action for add new tweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets';
 
 class NewTweet extends Component {
     state = {
@@ -17,16 +19,15 @@ class NewTweet extends Component {
         e.preventDefault()
 
         const { text } = this.state
+        const {dispatch, id} = this.props
 
-        // TODO: Add Tweet to Store
-
-        console.log('New Tweet: ', text)
+        dispatch(handleAddTweet(text, id))
 
         this.setState(() => ({
             text: ''
         }))
     }
-    
+
     render() {
         const { text } = this.state
 
@@ -63,4 +64,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
